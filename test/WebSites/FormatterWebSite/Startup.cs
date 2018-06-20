cd25247d7969f263c25e3761d9264090c4bea1f0
Kiran Challa <kichalla@microsoft.com>
2018-06-20 16:27:58 -0700

    Update functional testing websites to use dispatcher
    # Conflicts:
    #	test/Microsoft.AspNetCore.Mvc.FunctionalTests/DispatchingTests.cs

--- test/WebSites/FormatterWebSite/Startup.cs
+++ test/WebSites/FormatterWebSite/Startup.cs
@@ -12,6 +12,7 @@ namespace FormatterWebSite
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDispatcher();
             services.AddMvc(options =>
             {
                 options.ModelMetadataDetailsProviders.Add(new SuppressChildValidationMetadataProvider(typeof(Developer)));
@@ -27,10 +28,11 @@ namespace FormatterWebSite
 
         public void Configure(IApplicationBuilder app)
         {
-            app.UseMvc(routes =>
+            app.UseDispatcher();
+
+            app.UseMvcWithEndpoint(routes =>
             {
-                routes.MapRoute("ActionAsMethod", "{controller}/{action}",
-                    defaults: new { controller = "Home", action = "Index" });
+                routes.MapEndpoint("default", "{controller=Home}/{action=Index}");
             });
         }
     }
