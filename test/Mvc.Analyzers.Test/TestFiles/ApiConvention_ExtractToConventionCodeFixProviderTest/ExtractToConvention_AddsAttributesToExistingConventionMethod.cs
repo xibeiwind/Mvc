a311f8e6cd00985b77592ab923bd4eb0439aebc8
Pranav K <prkrishn@hotmail.com>
2018-07-10 10:17:42 -0700

    WIP
    WIP
    
    WIP

---
+++ test/Mvc.Analyzers.Test/TestFiles/ApiConvention_ExtractToConventionCodeFixProviderTest/ExtractToConvention_AddsAttributesToExistingConventionMethod.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
+namespace Microsoft.AspNetCore.Mvc.Analyzers
+{
+    [ApiConventionType(typeof(ExtractToConvention_AddsAttributesToExistingConventionMethodConvention))]
+    public class ExtractToConvention_AddsAttributesToExistingConventionMethod : ControllerBase
+    {
+        public ActionResult<string> GetPerson(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            else if (!User.IsInRole("SuperAdmin"))
+            {
+                return Unauthorized();
+            }
+
+            return string.Empty;
+        }
+    }
+
+    public static class ExtractToConvention_AddsAttributesToExistingConventionMethodConvention
+    {
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+        public static void Get(
+            [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Suffix)]
+            [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
+            object id)
+        { }
+
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+        public static void Post(
+            [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Any)]
+            [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
+            object model)
+        { }
+    }
+}
